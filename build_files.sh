b7debc49b01e1311a67ede06f276d2b05ba9d150
dzulfikarubaid <dzulfikarubaid@gmail.com>
2024-04-06 17:30:27 +0700

    initial commit

--- build_files.sh
+++ build_files.sh
@@ -3,4 +3,4 @@ source venv/bin/activate
 python3.9 -m pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput
 python3.9 -m pip install --upgrade pip
-python3.9 manage.py runserver
+
